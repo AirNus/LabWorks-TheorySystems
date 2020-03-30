44d30f7dbbaee0c76a430a3e7f5b84c951711f28
AirNus <58343285+AirNus@users.noreply.github.com>
2020-03-30 09:49:16 +0300

    Done
    Сделано

---
+++ LabWork(1)TheorySystem/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApplication2
+{
+    class Cipher
+    {
+        public string word { get; set; }
+        public string code { get; set; }
+        public double probability { get; set; }
+    }
+    class Program
+    {
+        static void EnterWord(List<Cipher> cipher)
+        {
+            Console.WriteLine("Введите текст, который нужно преобразовать в код");
+            string Text = Console.ReadLine();
+            int numerator; // Используется для расчета вхождений буквы в тексте 
+            char buffer; // Хранит в себе символы текста
+            for (int i = 0; i < Text.Length; i++)
+            {
+                numerator = 0;
+                buffer = Text[i];
+                for (int counter = 0; counter < cipher.Count; counter++)
+                {
+                    // Если данная буква уже была отработана
+                    if (buffer.ToString() == cipher[counter].word)
+                        buffer = '~';
+                }
+                for (int j = 0; j < Text.Length; j++)
+                {
+                    if (buffer == Text[j])
+                        numerator++;
+                }
+                if (buffer != '~')
+                {
+                    // Создаем новый объект который хранит данные текущей буквы
+                    Cipher row = new Cipher()
+                    {
+                        word = buffer.ToString(),
+                        probability = (double) numerator / Text.Length // вычисляем вероятность ее вхождения
+                    };
+                    // Добавляем отработанную букву и ее данные в список
+                    cipher.Add(row);
+                }
+            }
+        }
+        static void sort(List<Cipher> cipher)
+        {
+            var buffer = new Cipher(); // Создаем временную коллекцию для хранения элементов списка cipher
+            for (int i = 0; i < cipher.Count; i++)
+            {
+                for (int j = 0; j < cipher.Count; j++)
+                {
+                    // Если вероятность текущего элемента больше остальных
+                    if (cipher[i].probability > cipher[j].probability)
+                    {
+                        // Меняем элементы местами
+                        buffer = cipher[i];
+                        cipher[i] = cipher[j];
+                        cipher[j] = buffer;
+                    }
+                }
+            }
+
+        }
+        static void Union(ref List<Cipher> cipher)
+        {
+            List<Cipher> first_buffer = new List<Cipher>(cipher);           
+            int index_penult = 0, 
+                index_min = 0;
+            double min_probablty,
+                   penult_probablty;
+            // Начинаем обьединять буквы в группы по их частота
+            // Пока частота группы не равна 1
+            while (first_buffer.Count > 0 && first_buffer[0].probability < 1)
+            {
+                sort(first_buffer);
+                min_probablty = 1; // присваем переменной максимальную частоту для поиска буквы с минимальной частотой
+                for (int i = 0; i < first_buffer.Count; i++)
+                {
+                    // Ищем букву с  минимальной частотой
+                    if (first_buffer[i].probability <= min_probablty)
+                    {
+                        index_min = i;
+                        min_probablty = first_buffer[i].probability;
+                    }
+                }
+                penult_probablty = 1; // тоже самое только вместо минимальной ищем  ту что идет за ней либо равна минимальной
+                for (int i = 0; i < first_buffer.Count; i++)
+                {
+                    if (first_buffer[i].probability <= penult_probablty && i != index_min)
+                    {
+                        index_penult = i;
+                        penult_probablty = first_buffer[i].probability;
+                    }
+                }
+                for (int i = 0; i < cipher.Count; i++)
+                {
+                    // Если в сформированной группе букв есть текущая буква ей присвается код
+                    // по такому правилу
+                    // Минимальной присваивается 0
+                    // Следующей за минимальной присваивается 1
+                    if (first_buffer[index_min].word.Contains(cipher[i].word))
+                    {
+                        cipher[i].code += "0";
+                    }
+                    if (first_buffer[index_penult].word.Contains(cipher[i].word))
+                    {
+                        cipher[i].code += "1";
+                    }
+                }
+                first_buffer.Add(new Cipher
+                {
+                    probability = min_probablty + penult_probablty,
+                    word = first_buffer[index_min].word + first_buffer[index_penult].word
+                });
+                first_buffer.Remove(first_buffer[index_min]);
+                first_buffer.Remove(first_buffer[index_penult]);
+            }
+        }
+        public static void ReverseCode(List<Cipher> cipher)
+        {
+            char[] buff;
+            for (int i = 0; i < cipher.Count; i++)
+            {
+                buff = cipher[i].code.ToCharArray();
+                Array.Reverse(buff);
+                cipher[i].code = "";
+                for (int j = 0; j < buff.Length; j++)
+                {
+                    cipher[i].code += buff[j];
+                }
+            }
+        }
+        static void Main(string[] args)
+        {           
+            List<Cipher> cipher = new List<Cipher>();            
+            EnterWord(cipher);
+            sort(cipher);
+            Union(ref cipher);
+            ReverseCode(cipher);
+            foreach (var item in cipher)
+            {
+                Console.WriteLine("Символ: " + item.word + " преобразован в код: " + item.code);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LabWork(2)TheorySystem/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork_2_TheorySystem
+{
+    class Symbol
+    {
+        public char symbol;
+       public int frequency{ get; set; }
+       public string cipher { get; set; }
+
+    }
+    class Program
+    {
+        static List<Symbol> Fragmentation(string Text)
+        {
+            // Коллекция уникальных символов текста (содержит в себе обьекты класса Symbol)
+            List<Symbol> symbols = new List<Symbol>();
+            for (int i = 0; i < Text.Length; i++)
+            {              
+                // Если в списке уже есть такой символ увеличить frequency на 1                
+                var existsSymbolOnList = symbols.FirstOrDefault(x => x.symbol == Text[i]);
+                if (existsSymbolOnList != null)
+                {
+                    existsSymbolOnList.frequency++;
+                }
+                else
+                {
+                    var newSymbol = new Symbol { symbol = Text[i], frequency = 1 };
+                    symbols.Add(newSymbol);
+                }
+            }
+            return symbols;
+        }
+     
+        public static void OutputRezultInConsole(List<Symbol> symbols)
+        {
+            foreach (Symbol symbol in symbols)
+            {
+                Console.WriteLine(symbol.symbol + " - " + symbol.cipher);
+            }
+        }
+
+        static void SortList(List<Symbol> symbols)
+        {
+            int KolElementsList = symbols.Count; 
+            for(Boolean sorted = (KolElementsList == 0); sorted == false; KolElementsList --)
+            {
+                sorted = true;
+                for(int i = 1; i < KolElementsList; i++)
+                {
+                    if(symbols[i-1].frequency < symbols[i].frequency)
+                    {
+                        sorted = false;
+
+                        var buffer = symbols[i];
+                        symbols[i] = symbols[i - 1];
+                        symbols[i - 1] = buffer;
+                    }
+                }
+            }           
+        }
+
+        static void Main(string[] args)
+        {
+            List<Symbol> symbols = new List<Symbol>();
+            Console.WriteLine("Введите текст, который нужно зашифровать");
+            string Text = Console.ReadLine();
+            symbols = Fragmentation(Text);
+            SortList(symbols);
+            MethodShenonnaFano.Algoritm(symbols);
+            OutputRezultInConsole(symbols);
+            Console.ReadKey(true);
+        }
+    }
+
+    class MethodShenonnaFano
+    {
+        public static int CountSymbolsInList(List<Symbol> symbols)
+        {
+            int count = 0;
+            for (int i = 0; i < symbols.Count; i++)
+            {
+                count += symbols[i].frequency;
+            }
+            return count;
+        }
+        public static void Algoritm(List<Symbol> symbols)
+        {
+            List<Symbol> bufferOne = new List<Symbol>();
+            List<Symbol> bufferSecond = new List<Symbol>();
+            int delitel = 2;
+            int count = CountSymbolsInList(symbols);
+            int accum;
+            accum = 0;            
+            if(symbols.Count <= 1)
+            {
+                return;
+            }
+            for (int i = 0; i < symbols.Count; i++)
+            {
+                if (accum < count / delitel)
+                {
+                    symbols[i].cipher += "1";
+                    accum += symbols[i].frequency;
+                    bufferOne.Add(symbols[i]);
+                }
+                else
+                {
+                    bufferSecond.Add(symbols[i]);
+                    symbols[i].cipher += "0";
+                }
+            }
+            Algoritm(bufferOne);
+            Algoritm(bufferSecond);
+        }
+    }
+}
